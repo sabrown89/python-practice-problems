0faec337eb6910424bb0a4253cd2a0cb3801c7e1
scott brown <scottabrown89@gmial.com>
2018-02-16 14:04:59 -0500

    Adds basic url, views, index template

--- interviews/django-appointments/appointments/appointments/urls.py
+++ interviews/django-appointments/appointments/appointments/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
