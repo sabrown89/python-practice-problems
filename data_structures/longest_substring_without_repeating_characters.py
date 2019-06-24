8d8a9cb36e06d568604195bff8de9a5e09b678cd
scott brown <scottabrown89@gmial.com>
2019-06-24 19:44:48 -0400

    Found another bug with the way substrings are being handled with 'pwwkew' as a string

--- data_structures/longest_substring_without_repeating_characters.py
+++ data_structures/longest_substring_without_repeating_characters.py
@@ -1,15 +1,16 @@
 def longest_substring(string):
     substrings = []
     letter_index = {}
+    max_index_of_string = len(string) - 1
     for index, letter in enumerate(string):
-        import pdb
-        pdb.set_trace()
         if letter_index.get(letter) is not None:
             previous_index = letter_index[letter]
             substrings.append(string[previous_index:index])
             letter_index[letter] = index
         else:
             letter_index[letter] = index
-    if not substrings: # this solves the " " problem but not "aab", "ab" substring never gets added
+
+    # substrings for "pwwkew" are ["w", "wke"] which is incorrect
+    if not substrings:
         substrings.append(string)
     return max(len(letter) for letter in substrings)
