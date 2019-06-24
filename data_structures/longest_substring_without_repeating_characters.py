ec1448ce2d2c31d6a55eb958670f39557b3a2705
scott brown <scottabrown89@gmial.com>
2019-06-24 19:24:24 -0400

    Adds comment about why just adding the whole string is a problem

--- data_structures/longest_substring_without_repeating_characters.py
+++ data_structures/longest_substring_without_repeating_characters.py
@@ -2,10 +2,14 @@ def longest_substring(string):
     substrings = []
     letter_index = {}
     for index, letter in enumerate(string):
-        if letter_index.get(letter):
+        import pdb
+        pdb.set_trace()
+        if letter_index.get(letter) is not None:
             previous_index = letter_index[letter]
             substrings.append(string[previous_index:index])
             letter_index[letter] = index
         else:
             letter_index[letter] = index
+    if not substrings: # this solves the " " problem but not "aab", "ab" substring never gets added
+        substrings.append(string)
     return max(len(letter) for letter in substrings)
