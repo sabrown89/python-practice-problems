524502b93ba46fd954f60d31f0dc6c1d1f815983
scott brown <scottabrown89@gmial.com>
2020-04-09 10:13:50 -0400

    Removes example_pytest and kata_bank_ocr

--- tests/test_example_pytest.py
+++
@@ -1,5 +0,0 @@
-from example import example_pytest
-def test_add():
-    ad = example_pytest.Addition()
-    assert ad.add(1, 2) == 3
-
--- tests/test_kata_bank_ocr.py
+++
@@ -1,7 +0,0 @@
-from practice_problems import kata_bank_ocr
-
-kbo = kata_bank_ocr.KataBankOcr()
-
-def test_find_number():
-    f = open('kata_bank_one.txt', 'r')
-    assert kbo.find_number(f.read()) == 1
